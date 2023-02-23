1a2b81b738fe939f3bd332a556c35e5add159877
Atharv Parlikar <atharvparlikar@gmail.com>
2023-02-23 10:07:05 +0530

    added Value class and changed README.md

--- src/main.py
+++ src/main.py
@@ -2,6 +2,53 @@ import numpy as np
 from loss import MSEloss
 import activation
 
+
+class Value:
+    def __init__(self, value, children=()):
+        self.value = value
+        self.grad = 0
+        self._backward = lambda: None
+        self.children = children
+        self.prev = set(self.children)
+
+    def __add__(self, other):
+        out = Value(self.value + other.value, (self, other))
+
+        def _backward():
+            self.grad = out.grad
+            other.grad = out.grad
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.value * other.value, (self, other))
+
+        def _backward():
+            self.grad += out.grad * other.grad
+            other.grad += out.grad * self.grad
+        out._backward = _backward()
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v.prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1
+        for i in reversed(topo):
+            i._backward()
+
+    def __repr__(self):
+        return f'[{self.value} {self.grad}]'
+
+
 class NeuralNetwork:
     def __init__(self, inputs=0, outputs=0, hidden=[], add_biases=False, activation=None):
         self.input = inputs
@@ -9,28 +56,36 @@ class NeuralNetwork:
         self.hidden = hidden
         self.add_biases = add_biases
         self.activation = activation
-        self.biases = []
-
     # sets the weights randomly
+
+    def random(self, x, y=0):
+        mat = []
+        if y == 0:
+            return [Value(np.random.rand()) for _ in range(x)]
+        for i in range(y):
+            mat.append([Value(np.random.rand()) for _ in range(x)])
+        return mat
+
     def setWeights(self):
         if self.input != 0 and self.output != 0:
             if self.hidden == []:
-                self.weights = np.random.rand(self.output, self.input)
+                self.weights = self.random(self.output, self.input)
             elif len(self.hidden) == 1:
-                self.weights = [np.random.rand(self.hidden[0], self.input), np.random.rand(
+                self.weights = [self.random(self.hidden[0], self.input), self.random(
                     self.output, self.hidden[0]
                 )]
             else:
-                self.weights = [np.random.rand(self.hidden[0], self.input)]
+                self.weights = [
+                    self.random(self.hidden[0], self.input)]
                 for h in range(1, len(self.hidden)):
-                    self.weights.append(np.random.rand(
+                    self.weights.append(self.random(
                         self.hidden[h], self.hidden[h - 1]
                     ))
-                self.weights.append(np.random.rand(
+                self.weights.append(self.random(
                     self.output, self.hidden[-1]
                 ))
         if self.add_biases:
-            self.biases = np.random.rand(len(self.weights))
+            self.biases = self.random(len(self.weights))
 
     def dotproduct(self, layer, weights):
         product = []
@@ -43,7 +98,8 @@ class NeuralNetwork:
         if self.add_biases:
             if self.activation != None:
                 for (weight, bias) in zip(self.weights, self.biases):
-                    last_layer = self.activation(self.dotproduct(last_layer, weight) + bias)
+                    last_layer = self.activation(
+                        self.dotproduct(last_layer, weight) + bias)
             else:
                 for (weight, bias) in zip(self.weights, self.biases):
                     last_layer = self.dotproduct(last_layer, weight) + bias
@@ -56,15 +112,15 @@ class NeuralNetwork:
 
 
 # Example of forward a pass
-nn = NeuralNetwork()
-nn.input = 28*28
-nn.output = 2
-nn.hidden = [2, 2]
-nn.add_biases = True
-nn.setWeights()
-input_layer = np.random.rand(28*28)
-forward = nn.forward(input_layer)
-nn.activation = activation.tanh
-forward_with_activation = nn.forward(input_layer)
-print(forward)
-print(forward_with_activation)
+# nn = NeuralNetwork()
+# nn.input = 2
+# nn.output = 1
+# nn.hidden = [3]
+# nn.add_biases = True
+# nn.setWeights()
+# input_layer = [Value(np.random.rand()) for _ in range(2)]
+# forward = nn.forward(input_layer)
+# nn.activation = activation.tanh
+# forward_with_activation = nn.forward(input_layer)
+# print(forward)
+# print(forward_with_activation)
